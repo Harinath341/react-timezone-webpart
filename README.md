5b8956575ad2be87b5d99b2a59c34cb449d8da99
ST Chiew <st.chiew@gmail.com>
2019-06-14 22:32:44 +0800

    release 1.0.2

--- README.md
+++ README.md
@@ -6,11 +6,14 @@ SPFx webpart that displays a selectable date/time and displays it in different t
 
 | Date     | Version | Comments        |
 | -------- | ------- | --------------- |
+| Jun 2019 | 1.0.2   | next release    |
 | May 2019 | 1.0.1   | Initial release |
 
-### Features
+### Release 1.0.2
 
-- Display current local date
-- Display date picker
-  - 1
-  - 2
+- Display current date/time in current timezone
+- DIsplay current date/time in another timezone
+
+### Release 1.0.1
+
+- Baseline release
--- src/webparts/timeZone/components/TimeZone.tsx
+++ src/webparts/timeZone/components/TimeZone.tsx
@@ -1,24 +1,23 @@
-import * as React from "react";
-import styles from "./TimeZone.module.scss";
-import { ITimeZoneProps } from "./ITimeZoneProps";
-import { escape } from "@microsoft/sp-lodash-subset";
+import * as React from 'react';
+import styles from './TimeZone.module.scss';
+import { ITimeZoneProps } from './ITimeZoneProps';
+import Clock from 'react-live-clock';
+
+const curr_timedate = new Date();
 
 export default class TimeZone extends React.Component<ITimeZoneProps, {}> {
-  public render(): React.ReactElement<ITimeZoneProps> {
-    return (
-      <div className={styles.timeZone}>
-        <div className={styles.container}>
-          <div className={styles.row}>
-            <div className={styles.column}>
-              <span className={styles.title}>Welcome to SharePoint!</span>
-              <p className={styles.subTitle}>An SPFx Timezone display.</p>
-              <p className={styles.description}>
-                {escape(this.props.description)}
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+	public render(): React.ReactElement<ITimeZoneProps> {
+		return (
+			<div className={styles.timeZone}>
+				<div className={styles.container}>
+					<div>
+						<Clock format={'HH:mm:ss'} ticking={true} />
+					</div>
+					<div>
+						<Clock format={'HH:mm:ss'} ticking={true} timezone={'America/Chicago'} />
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
